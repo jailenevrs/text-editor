d855694d435d928f02c79f79c9a0fc51f7cd8553
Jailene <jaileneveras8@gmail.com>
2023-02-28 13:16:23 -0500

    added workbox plugin to config.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,7 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
+const WorkboxPlugin = require ('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
@@ -17,11 +18,16 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    devServer:{
+      hot: "only"
+    },
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin'
-      })
+      }),
+      new MiniCssExtractPlugin(),
+      new WorkboxPlugin.GenerateSW()
       
     ],
 
