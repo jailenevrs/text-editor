cf58b43fec6e520b07b034a3fd8e114f006f1dc0
Jailene <jaileneveras8@gmail.com>
2023-02-28 14:02:08 -0500

    Added function to database.js to add data to index.db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -11,9 +11,22 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
-
+  export const postDb = async (content) => {
+    console.log('post to database')
+  
+  const jateDb = await openDB('jate', 1 );
+  const tx = jateDb.transaction('jate', 'readwrite')
+  const store = tx.objectStore('jate');
+  const request = store.add({jate : content})
+  const result = await request;
+  console.log ("data saved to db" , result);
+  }
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+
+}
+  
+//   console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
