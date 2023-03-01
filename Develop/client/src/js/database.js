b3b618f74dca9d078f9c4d58deac83a1de8b7a81
Jailene <jaileneveras8@gmail.com>
2023-03-01 14:36:10 -0500

    finished webpack and service worker configuration

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -11,21 +11,17 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
-  export const postDb = async (content) => {
-    console.log('post to database')
+  export const putDb = async (content) => {
+    console.log('put to database')
   
   const jateDb = await openDB('jate', 1 );
   const tx = jateDb.transaction('jate', 'readwrite')
   const store = tx.objectStore('jate');
-  const request = store.add({jate : content})
+  const request = store.put({id : 1, value: content})
   const result = await request;
-  console.log ("data saved to db" , result);
+  console.log ("data saved to db" , result.value);
   }
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => { 
-
-}
-  
 //   console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -33,10 +29,10 @@ export const getDb = async () => {
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
-  console.log('result.values', result);
-
+  console.log('data retrieved', result.value);
+return result.value
 }
 
 // }console.error('getDb not implemented');
